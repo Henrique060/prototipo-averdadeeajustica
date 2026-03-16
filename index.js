cde383fcf7f9a9166e560d4cd65e7540864ce3fc
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-03-16 16:43:57 +0000

    different port for start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.use(express.static("dist"));
 
