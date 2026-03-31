4ee8cc22f42bc6b9d8edfc06ea45db5332600c75
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-03-31 18:02:05 +0100

    new design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,29 +5,13 @@ import './Home.css'; // We will move your CSS here
 function Home() {
   return (
     <div className="home-wrapper">
-      
+      <section className="hero">
+        
+        <Link className="hero-btn" to="/tutorial">
+          Iniciar Experiência
+        </Link>
 
-      <main className="home-main">
-        {/* React handles custom elements like <model-viewer> automatically */}
-
-        <div className="container-flex">
-        <div className="container-flex-item-3">
-        </div>
-      </div>
-
-      <section className='buttons-section'>
-        <div className="button-row">
-            <Link className="btnNavEmpty" to="/">
-            </Link>
-            <Link className="btnNav btnNav-secondary" to="/tutorial">
-              <img src="/icons/front-arrow.svg" alt="Seguinte" />
-              
-            </Link>
-        </div>
-        </section>
-
-          
-      </main>
+      </section>
     </div>
   );
 }
