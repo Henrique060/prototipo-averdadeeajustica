b508d8d52e096cfdbae28aa68c55ce654a6ffaa9
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-04-01 15:22:58 +0100

    desktop landing page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'; // We will move your CSS here
+import './Home.css';
 
 function Home() {
   return (
     <div className="home-wrapper">
-      <section className="hero">
-        
+
+      {/* MOBILE / PORTRAIT VERSION */}
+      <section className="hero hero-mobile">
         <Link className="hero-btn" to="/tutorial">
           Iniciar Experiência
         </Link>
+      </section>
+
+      {/* DESKTOP / LANDSCAPE VERSION */}
+      <section className="hero-desktop">
+
+        <div className="hero-left"></div>
+
+        <div className="hero-right">
+          <h1>Aceda à experiência no seu telemóvel</h1>
+
+          <img
+            src="/images/qrcode.jpeg"
+            alt="QR Code"
+            className="qr-code"
+          />
+
+          <p>
+            Se estiver a usar um tablet, coloque na orientação vertical
+          </p>
+
+          <Link className="hero-btn" to="/tutorial">
+            Iniciar Experiência
+          </Link>
+        </div>
 
       </section>
+
     </div>
   );
 }
