b40e8107273876e276234fe66557aff88d54691b
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-04-02 15:15:14 +0100

    gradient blur added to bottom of mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { IoHomeOutline, IoBookOutline, IoMapOutline, IoListOutline, IoArrowBackOutline, IoArrowForwardOutline } from "react-icons/io5";
 import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav className="bottom-nav">
+      <NavLink to="/" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+        <IoArrowBackOutline size={24} />
+      </NavLink>
+      
+      <NavLink to="/tutorial" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+        <IoBookOutline size={24} />
+      </NavLink>
+      
+      <NavLink to="/map" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+        <IoMapOutline size={24} />
+      </NavLink>
+      
+      <NavLink to="/list" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+        <IoListOutline size={24} />
+      </NavLink>
+      
+      {/* For 'Back', you might want to use a button or a specific route */}
+      <NavLink to="/settings" className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}>
+        
+        <IoArrowForwardOutline size={24} />
+      </NavLink>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Tutorial.css';
 import PageHeader from '../components/PageHeader';
+import NavBar from '../components/NavBar';
 
 function Tutorial() {
   return (
     <div className="home-wrapper">
       <PageHeader title="Como funciona A VERDADE e a J-U-S-T-I-Ç-A?" />
+      <NavBar />
     </div>
   );
 }
