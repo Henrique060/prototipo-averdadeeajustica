d2c2469e6cef7f63422fb8d4fc7435bc07e5e586
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-06-09 18:11:26 +0100

    asset target changed

--- src/pages/Escadaria.jsx
+++ src/pages/Escadaria.jsx
@@ -16,7 +16,7 @@ function Escadaria() {
       mapImgSrc="/images/mapa-museu.webp"
       targetImg="/markers/terreiro-militar-marker.mind"
       assets={[
-        "/images/sala21.webp", null, null
+        "/images/terreiro-militar.jpeg", null, null
       ]}
     />
   );
