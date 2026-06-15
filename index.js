14bcdc8895c4f85cd04d271c18b914ba9755c411
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-06-15 20:21:58 +0100

    trying new index js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import path from "path";
+import compression from "compression"; // 1. Add this import
 
 const app = express();
+
+app.use(compression()); // 2. Add this line BEFORE your static paths
+app.use(express.static(path.join(__dirname, "dist")));
 const __dirname = new URL('.', import.meta.url).pathname;
 
 app.use(express.static(path.join(__dirname, "dist")));
