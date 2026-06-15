6e8b1b90ad58154d872c60d2cadbd8eee719e285
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-06-15 19:23:28 +0100

    base vite config updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   assetsInclude: ['**/*.mind'],
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
