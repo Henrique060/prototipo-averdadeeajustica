63fd0bef20ad46dbead7d8a1922fd34288919bf2
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-07-20 15:46:29 +0100

    final page updated with the ar button and end button to redirect to project page

--- src/pages/FinalPage.jsx
+++ src/pages/FinalPage.jsx
@@ -26,7 +26,8 @@ function FinalPage () {
                 </div>
     
                     <div className="model-viewer-div">
-                        <model-viewer src="/models/monumentoajustica.glb" shadow-intensity="1"></model-viewer>
+                        <model-viewer src="/models/monumentoajustica.glb" shadow-intensity="1" ar 
+                        touch-action="pan-y"></model-viewer>
                     </div>
 
                     <p className="experience-title-question">Monumento à Justiça finalizado!</p>
@@ -37,11 +38,14 @@ function FinalPage () {
                     <p className="experiencie-list-text">
                     Dentro do monumento está uma mesa, coloque o objeto, dentro dos presentes, que acha que mais apropriado estar em cima da mesma.
                     </p>
+                    <button className="mv-ar-btn" slot="ar-button">
+                                Entre na experiência
+                    </button>
             </div>
         </div>
         <div className="quadro-container-button-wrapper">
           {/* Replaced old local state button logic with navigation trigger */}
-          <button className="quadro-container-button"onClick={() => navigate("/thesis-project-page")}>Terminar Experiência</button>
+          <button className="final-container-button"onClick={() => navigate("/thesis-project-page")}>Terminar</button>
         </div>
     </div>
     );
