3f3dfa278ec36c115c171b56fde4028c158932e5
Henrique <77362998+Henrique060@users.noreply.github.com>
2026-09-09 19:16:30 +0100

    added useeffect for experiences

--- src/pages/LencoSaudade.jsx
+++ src/pages/LencoSaudade.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';import React from 'react';
 import ExperiencePageComponent from '../components/ExperiencePageComponent.jsx';
 
 function LencoSaudade() {
